4d260007155205130cbc35095437d8374391c572
AndreMasterRace <andre7rufo@gmail.com>
2019-11-06 23:04:35 +0000

    Added Inputs for movement and spill
     - new scene;
     - moved assets to folders;
     - created scripts for inputs.

---
+++ Assets/Scripts/SpillController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpillController : MonoBehaviour
+{
+    ParticleSystem ps;
+    private void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ps.Play();
+        }
+    }
+}
