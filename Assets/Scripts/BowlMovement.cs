6703a49955205d0acbba2d062c84233e5348777d
AndreRufo <andre7rufo@gmail.com>
2019-12-07 17:13:48 +0000

    Gyroscope Movement - Rotation
     - device Rotation achieved.

--- Assets/Scripts/BowlMovement.cs
+++ Assets/Scripts/BowlMovement.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class BowlMovement : MonoBehaviour
 {
 
-    public float movementSpeed = 5.0f;
+    public float movementSpeed = 2.0f;
+    public float maxSpeed = 10.0f;
     public float rotationSpeed = 200.0f;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Input.gyro.enabled = true;
+    }
 
     void Update()
     {
+        Quaternion deviceRotation = DeviceRotation.Get();
+        transform.rotation = deviceRotation;
+
+        //rb.AddForce(transform.forward * movementSpeed, ForceMode.Acceleration);
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
         //transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0);
         //transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed);
-        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime * movementSpeed); 
+       // transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime * movementSpeed);
+
 
     }
 }
